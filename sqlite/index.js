698b11267f25663e9987fbde4074e997bc5d0f87
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-02-25 16:13:35 +0000

    Updating - Moving SQLite into Folder

--- sqlite/index.js
+++ sqlite/index.js
@@ -7,22 +7,28 @@ const db = new sqlite3.Database("./mock.db", sqlite3.OPEN_READWRITE, (error) => 
 });
 
 // db.run('CREATE TABLE products(id, title, type)');
+const tables =
+`
+
+)`;
+const sql = `SELECT sql FROM sqlite_schema WHERE name = 'accounts'`;
+
 // const sql = `INSERT INTO products (id, title, type)
               // VALUES(?,?,?)`;
-const sql = `SELECT * FROM products`;
-
 
 // db.run(sql, [1, 'SkyMaster 25x70', 'Binoculars'], (error) => {
 //   if (error) return console.error(error.message);
 
 //   console.log('A new row had bin added');
 // });
-db.all(sql, [], (err, rows) => {
-  if (err) return console.error(err.message);
 
-  rows.forEach(row => {
-    console.log(row);
-  });
+db.all(
+`
+SELECT * FROM accounts
+`, [], (err, row) => {
+  if(err) return console.error(err.message);
+
+  console.log(row);
 })
 
 db.close((error) => {
