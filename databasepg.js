41c373bf2e4c2a543693d009f605539b3b8999ca
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-02-27 12:09:13 +0000

    Adding - Testing Done

--- databasepg.js
+++ databasepg.js
@@ -10,7 +10,31 @@ const client = new Client({
 
 client.connect();
 
+client.query('SELECT * FROM orders', (err, res) => {
+  if (err) return console.error(err.message);
+
+  console.log(res.rows);
+  
+  // client.end();
+});
+
+client.query('SELECT * FROM products', (err, res) => {
+  if (err) return console.error(err.message);
+
+  console.log(res.rows);
+  
+  // client.end();
+});
+
 client.query('SELECT * FROM accounts', (err, res) => {
+  if (err) return console.error(err.message);
+
+  console.log(res.rows);
+  
+  // client.end();
+});
+
+client.query('SELECT * FROM cart', (err, res) => {
   if (err) return console.error(err.message);
 
   console.log(res.rows);
