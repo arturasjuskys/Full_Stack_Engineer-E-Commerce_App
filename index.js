28eeb876b03d57a46c067c4be35974ef4255f653
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-03-05 15:27:07 +0000

    Deleting - All Files & Adding Express Server

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-// to include .env variables
-require('dotenv').config();
 const express = require('express');
+
 const app = express();
+const PORT = 4000;
 
-const { PORT } = require('./config');
-console.log(process.env.PORT);
-async function server() {
-
-  // starting express server
-  app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}`);
-  })
-};
-
-server();
+// Express Server
+app.listen(PORT, () => {
+  console.log(`Express server is listening on PORT: ${PORT}`);
+});
