2c45d29689a98a046a64e6ef2a9ee378d9aa9447
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-03-05 22:57:32 +0000

    Adding - Server & index

---
+++ server.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+const express = require('express');
+
+const dotenv = require('dotenv').config();
+const app = require('./index');
+const PORT = process.env.PORT || 4001;
+
+// PostgreSQL Server Connection
+const pool = new Pool({
+  user: 'postgres',
+  password: 'postgres',
+  database: 'ecommerce',
+  host: 'localhost',
+  port: 5432
+});
+
+// Express Server
+app.use(express.json());
+app.listen(PORT, () => {
+  console.log(`Express server is listening on PORT: ${PORT}`);
+});
+
+module.exports = pool;
