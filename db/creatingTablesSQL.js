7e2fe65f28ed817d9425dc269d258bd3b6386f84
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-03-30 18:50:31 +0100

    Updating - Text Formating

--- db/creatingTablesSQL.js
+++ db/creatingTablesSQL.js
@@ -3,8 +3,8 @@ module.exports.createUsersTable = `
     id              INT               PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     email           VARCHAR(50),      
     password        TEXT,
-    first_name       VARCHAR(50),
-    last_name        VARCHAR(50),
+    first_name      VARCHAR(50),
+    last_name       VARCHAR(50),
     google          JSON,
     facebook        JSON
   );
@@ -22,7 +22,7 @@ module.exports.createOrdersTable = `
     id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     total           INT             NOT NULL,
     status          VARCHAR(50)     NOT NULL,
-    user_id          INT             NOT NULL,
+    user_id         INT             NOT NULL,
     created         DATE            NOT NULL,
     modified        DATE            NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
@@ -32,10 +32,10 @@ module.exports.createOrderItemsTableS = `
   CREATE TABLE IF NOT EXISTS order_items (
     id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     created         DATE            NOT NULL,
-    order_id         INT             NOT NULL,
+    order_id        INT             NOT NULL,
     qty             INT             NOT NULL,
     price           INT             NOT NULL,
-    product_id       INT             NOT NULL,
+    product_id      INT             NOT NULL,
     name            VARCHAR(50)     NOT NULL,
     description     VARCHAR(200)    NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(id)
@@ -44,7 +44,7 @@ module.exports.createOrderItemsTableS = `
 module.exports.createCartsTable = `
   CREATE TABLE IF NOT EXISTS carts (
     id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-    user_id          INT             NOT NULL,
+    user_id         INT             NOT NULL,
     modified        DATE            NOT NULL,
     created         DATE            NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
@@ -53,8 +53,8 @@ module.exports.createCartsTable = `
 module.exports.createCartItemsTable = `
   CREATE TABLE IF NOT EXISTS cart_items (
     id              INT             PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-    cart_id          INT             NOT NULL,
-    product_id       INT             NOT NULL,
+    cart_id         INT             NOT NULL,
+    product_id      INT             NOT NULL,
     qty             INT             NOT NULL,
     FOREIGN KEY (cart_id) REFERENCES carts(id),
     FOREIGN KEY (product_id) REFERENCES products(id)
