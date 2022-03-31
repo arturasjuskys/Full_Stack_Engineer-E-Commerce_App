5be965e53820065400bad2bf0ace09c6690e73e0
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-03-31 19:14:21 +0100

    Adding - Add to Cart Endpoint

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const productRouter = require('./routes/productRouter');
 const orderRouter = require('./routes/orderRouter');
 const userRouter = require('./routes/userRouter');
 const cartRouter = require('./routes/cartRouter');
+const cartItemRouter = require('./routes/cartItemRouter');
 
 app.use(express.json());
 
 // Routes
 app.use('/products', productRouter);
 app.use('/account', orderRouter);
-app.use('/account', cartRouter)
+app.use('/account', cartRouter);
+app.use('/account', cartItemRouter);
 app.use('/users', userRouter);
 app.use('/orders', orderRouter);
 app.use('/carts', cartRouter)
--- models/cartItemModel.js
+++ models/cartItemModel.js
@@ -2,13 +2,26 @@ const db = require('../db');
 
 module.exports = class CartItemModel {
   // Create
+  async createOne(cart_id, product_id, qty) {
+    const sql = `
+      INSERT INTO cart_items (cart_id, product_id, qty) VALUES ($1, $2, $3) RETURNING *;
+    `;
+
+    try {
+      const cartItem = await db.query(sql, [cart_id, product_id, qty]);
+
+      return cartItem.rows[0];
+    } catch (error) {
+      throw error;
+    }
+  }
 
   // Update
 
   // Get all
 
   // Delete
-  static async deleteOne(id) {
+  async deleteOne(id) {
     const sql = `
     DELETE FROM cart_items
     WHERE id = $1
